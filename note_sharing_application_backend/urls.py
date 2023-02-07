c3f72282a79463db72b4e3dafdba5df2d354bf6f
Tanmoy Saha <tanmoytssaha@gmail.com>
2023-02-07 21:29:39 +0530

    API created for profile picture

--- note_sharing_application_backend/urls.py
+++ note_sharing_application_backend/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_page
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home_page),
     path('api/v1/', include('user_profile.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- user_profile/models.py
+++ user_profile/models.py
@@ -4,14 +4,15 @@ from django.db import models
 class Courses(models.Model):
     courses = models.CharField(max_length= 100, blank=True)
 
-    def __unicode__(self):
+    def __str__ (self):
         return self.courses
 
 class UserProfile(models.Model):
     user_id = models.AutoField(primary_key = True)
-    imgUrl = models.TextField()
+    img = models.ImageField(upload_to='profile_picture', blank=True)
     user_name = models.CharField(max_length=100)
     user_description = models.TextField()
     university_college = models.CharField(max_length=100)
     course = models.ForeignKey(Courses, blank=True, null=True, on_delete=models.CASCADE)
-    
+    created_at = models.DateTimeField(auto_now_add=True)
+    address = models.CharField(max_length=200)
